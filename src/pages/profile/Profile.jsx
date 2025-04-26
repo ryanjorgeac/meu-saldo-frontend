49281cd3ff10695bbc793f1f84f571bcc4430a9f
HeinrichDev6002 <tutorgames.gmil.com@gmail.com>
2025-04-26 19:47:15 -0300

    feat: novas alterações

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,21 +1,41 @@
+import { useState } from "react";
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
+import "./profile.css";
+
 function Profile() {
+  const { user, logout } = useContext(AuthContext);
+  const [name, setName] = useState(user ? user.name : "usuário");
   return (
-    <div>
-      <img
-        src="/src/assets/user-profile.png"
+    <>
+      <div id="profile-div">
+      <div id="img-div">
+        <img
+        src="/src/assets/profile-icon.png"
         alt="generic user png"
         id="user-img"
       ></img>
-      <h2>Perfil</h2>
+      </div>
+
+      <h2>{name}</h2>  
+      
+      <div>
+      <p className="user-data">{name} </p>
+            
+      <div id="button-div">
+
+      <button id="logout" onClick={logout}>Sair/Deslogar</button>
+      
+      </div>
       <br></br>
-      <p>Nome</p>
-      <br></br>
-      <p>Renda</p>
-      <br></br>
-      <p>Ocupação</p>
-      <br></br>
+      </div>
+      
     </div>
+    </>
+    
   );
 }
 
+
 export default Profile;
+
