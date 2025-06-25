40bbdfb31b56494fb24b4a06100e1cc41bc1a9bf
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2025-06-25 11:21:44 -0300

    refactor logo div and nav div to match figma design

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,34 +1,37 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import "./Header.css";
-import DropDown from "../../pages/DropDown/DropDown";
+import DropDown from "../profileMenu/DropDown";
 
 function Header() {
   return (
     <header className="header">
       <div className="header-content">
-        <div className="logo-title">
-          <img src="/src/assets/logo.svg" alt="Logo" className="logo" />
-          <h1 className="title">Meu Saldo</h1>
+        <Link to="/dashboard" className="logo-title-link">
+          <div className="logo-title">
+            <img src="/src/assets/logo.svg" alt="Logo" className="logo" />
+            <h1 className="title">Meu Saldo</h1>
+          </div>
+        </Link>
+
+        <div className="nav-container">
+          <nav className="nav-buttons">
+            <NavLink to="/dashboard" className="nav-link">
+              Dashboard
+            </NavLink>
+            <NavLink to="/categories" className="nav-link">
+              Categorias
+            </NavLink>
+            <NavLink to="/transactions" className="nav-link">
+              Transações
+            </NavLink>
+            <NavLink to="/analytics" className="nav-link">
+              Análises
+            </NavLink>
+          </nav>
         </div>
 
-        <nav className="nav-buttons">
-          <NavLink to="/dashboard" className="nav-link">
-            Dashboard
-          </NavLink>
-          <NavLink to="/categories" className="nav-link">
-            Categorias
-          </NavLink>
-          <NavLink to="/transactions" className="nav-link">
-            Transações
-          </NavLink>
-          <NavLink to="/analytics" className="nav-link">
-            Análises
-          </NavLink>
-        </nav>
         <div className="profile-container">
-         <DropDown/>
-           
-         
+         <DropDown/>         
         </div>
 
       </div>
