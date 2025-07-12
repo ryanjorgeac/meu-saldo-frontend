4d06568847ca744599b95bc6c6a4bcf1cbe03e30
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2025-07-12 10:43:58 -0300

    adding default arguments to not break existing use cases and preserve usability of the component

--- src/components/auth/ActionButton.jsx
+++ src/components/auth/ActionButton.jsx
@@ -1,9 +1,16 @@
-export default function ActionButton({ text, disabled = false, isLoading = false }) {
+export default function ActionButton({ 
+  text, 
+  disabled = false, 
+  isLoading = false, 
+  onClick = null,
+  type = "submit" 
+}) {
   return (
     <button 
-      type="submit" 
+      type={type}
       className="auth-submit-button" 
       disabled={disabled || isLoading}
+      onClick={onClick}
     >
       <div className="auth-button-content">
         {isLoading && <div className="auth-button-spinner"></div>}
