70021ed7ea3fabef1d1ad930372bff3fc2ca6ad3
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2025-07-22 16:19:46 -0300

    implement budget summary and use Inter font correctly

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import "inter-ui/inter.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
